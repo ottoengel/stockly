a2a4798f51c2de75ea8f3e1dcc9eda61a50de766
Otto <ottoengelhardt18@gmail.com>
2024-09-24 20:40:36 -0300

    refactor: add ProductTableDropDownMenu component

---
+++ app/products/_components/table-dropdown-menu.tsx
@@ -0,0 +1,82 @@
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
+import { Button } from "@/app/_components/ui/button";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+import { Product } from "@prisma/client";
+import {
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  TrashIcon,
+} from "lucide-react";
+import { useState } from "react";
+import UpsertProductDialogContent from "./upsert-dialog-content";
+import DeleteProductDialogContent from "./delete-dialog-content";
+
+interface ProductTableDropDownMenuProps {
+  product: Product;
+}
+
+const ProductTableDropDownMenu = ({
+  product,
+}: ProductTableDropDownMenuProps) => {
+  const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
+  return (
+    <AlertDialog>
+      <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DialogTrigger asChild>
+              <DropdownMenuItem className="gap-1.5">
+                <EditIcon size={16} />
+                Editar
+              </DropdownMenuItem>
+            </DialogTrigger>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem className="gap-1.5">
+                <TrashIcon size={16} />
+                Deletar
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UpsertProductDialogContent
+          defaultValues={{
+            id: product.id,
+            name: product.name,
+            price: Number(product.price),
+            stock: product.stock,
+          }}
+          onSuccess={() => setEditDialogIsOpen(false)}
+        />
+        <DeleteProductDialogContent productId={product.id} />
+      </Dialog>
+    </AlertDialog>
+  );
+};
+
+export default ProductTableDropDownMenu;
