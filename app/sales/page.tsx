170642c4b9d098fe1b3109f5324b77fc46f97b85
Otto <ottoengelhardt18@gmail.com>
2024-12-02 19:04:25 -0300

    feat: ensure sales are listed

--- app/sales/page.tsx
+++ app/sales/page.tsx
@@ -1,8 +1,12 @@
 import { ComboboxOption } from "../_components/ui/combobox";
+import { DataTable } from "../_components/ui/data-table";
 import { getProducts } from "../_data-access/product/get-products";
+import { getSales } from "../_data-access/sale/get-sales";
 import CreateSaleButton from "./_components/create-sale-button";
+import { saleTableColumns } from "./_components/table-columns";
 
 const SalesPage = async () => {
+  const sales = await getSales();
   const products = await getProducts();
   const productOptions: ComboboxOption[] = products.map((product) => ({
     label: product.name,
@@ -15,11 +19,11 @@ const SalesPage = async () => {
           <span className="text-xs font-semibold text-slate-500">
             Gestão de Vendas
           </span>
-          <h2 className="text-xl font-semibold">Produtos</h2>
+          <h2 className="text-xl font-semibold">Vendas</h2>
         </div>
         <CreateSaleButton products={products} productOptions={productOptions} />
       </div>
-      {/* <DataTable columns={productTableColumns} data={products} /> */}
+      <DataTable columns={saleTableColumns} data={sales} />
     </div>
   );
 };
