14953a889fcfc1893c48d3471cb2e47abfdb1844
Otto <ottoengelhardt18@gmail.com>
2024-12-02 23:50:24 -0300

    refactor: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Header, {
+  HeaderLeft,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "./_components/header";
+
 const Home = () => {
-  return ( 
-    <div>
-      <h1>Home Page</h1>
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Visão geral</HeaderSubtitle>
+          <HeaderTitle>DashBoard</HeaderTitle>
+        </HeaderLeft>
+      </Header>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Home;
