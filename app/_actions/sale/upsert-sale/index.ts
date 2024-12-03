598c6a2db876228e5e2da3e1af58c95bb2308485
Otto <ottoengelhardt18@gmail.com>
2024-12-03 19:50:09 -0300

    feat: ensure actions revalidate pages properly

--- app/_actions/sale/upsert-sale/index.ts
+++ app/_actions/sale/upsert-sale/index.ts
@@ -74,7 +74,5 @@ export const upsertSale = actionClient
         });
       }
     });
-    revalidatePath("/products");
-    revalidatePath("/sales");
-    revalidatePath("/");
+    revalidatePath("/", "layout");
   });
