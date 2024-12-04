88eb7e6ceaa6c5e0e9b6e9996419e4b5d659999e
Otto <ottoengelhardt18@gmail.com>
2024-12-03 21:01:06 -0300

    feat: add initial revenue chart

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -1,14 +1,50 @@
-import { db } from "@/app/_lib/prisma";
+import "server-only";
 
-export interface DashboardDto {
+import { db } from "@/app/_lib/prisma";
+import dayjs from "dayjs";
+
+export interface DayTotalRevenue {
+  day: string;
+  totalRevenue: number;
+}
+
+interface DashboardDto {
   totalRevenue: number;
   todayRevenue: number;
   totalSales: number;
   totalStock: number;
   totalProducts: number;
+  totalLast14DaysRevenue: DayTotalRevenue[];
 }
 
 export const getDashboard = async (): Promise<DashboardDto> => {
+  //pegar as receitas de 14 dias atrás até o final do dia de hoje
+  const today = dayjs().endOf("day").toDate();
+  const last14Days = [13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0].map(
+    (day) => {
+      return dayjs(today).subtract(day, "day");
+    },
+  );
+  //guardadr os dados dos ultimos dias
+  const totalLast14DaysRevenue: DayTotalRevenue[] = [];
+  for (const day of last14Days) {
+    const dayTotalRevenue = await db.$queryRawUnsafe<
+      { totalRevenue: number }[]
+    >(
+      `
+        SELECT SUM("unityPrice" * "quantity") as "totalRevenue"
+        FROM "SaleProduct"
+        WHERE "createdAt" >= $1 AND "createdAt" <= $2;
+        `,
+      day.startOf("day").toDate(),
+      day.endOf("day").toDate(),
+    );
+    totalLast14DaysRevenue.push({
+      day: day.format("DD/MM"),
+      totalRevenue: dayTotalRevenue[0].totalRevenue,
+    });
+  }
+
   //pegar todos os produtos vendidos e somar o valor total
   const totalRevenueQuery = `
     SELECT SUM("unityPrice" * "quantity") as "totalRevenue"
@@ -55,5 +91,6 @@ export const getDashboard = async (): Promise<DashboardDto> => {
     totalSales,
     totalStock: Number(totalStock._sum.stock),
     totalProducts,
+    totalLast14DaysRevenue,
   };
 };
