3a1b70651dbed58481ad9cb22d8afdd29790d98f
Otto <ottoengelhardt18@gmail.com>
2024-12-05 20:31:40 -0300

    feat: ensure sales and products page are scrollable

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -13,7 +13,7 @@ const ProductsPage = async () => {
   //chamar o banco de dados
   const products = await getProducts();
   return (
-    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+    <div className="m-8 w-full space-y-8 overflow-auto rounded-lg bg-white p-8">
       <Header>
         <HeaderLeft>
           <HeaderSubtitle>Gestão de Produtos</HeaderSubtitle>
