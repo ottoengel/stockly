e5a4b7791cff7051feaaae47a7d0a321be461eb6
Otto <ottoengelhardt18@gmail.com>
2024-12-05 15:37:47 -0300

    feat: ensure most sold products are shown on dashboard

--- app/_data-access/product/get-products.ts
+++ app/_data-access/product/get-products.ts
@@ -2,8 +2,10 @@ import "server-only";
 import { db } from "@/app/_lib/prisma";
 import { Product } from "@prisma/client";
 
+export type ProductStatusDto = "IN_STOCK" | "OUT_OF_STOCK";
+
 export interface productDto extends Product {
-  status: "IN_STOCK" | "OUT_OF_STOCK";
+  status: ProductStatusDto;
 }
 
 export const getProducts = async (): Promise<productDto[]> => {
